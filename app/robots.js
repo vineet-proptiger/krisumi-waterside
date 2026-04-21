5172eca3e8e257ec87631f3651fc39569bcc3432
vineet-proptiger <vineet.mishra@proptiger.com>
2026-04-21 16:32:54 +0530

    feat: add sitemap.js and robots.js for SEO configuration

---
+++ app/robots.js
@@ -0,0 +1,10 @@
+export default function robots() {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+      disallow: '',
+    },
+    sitemap: 'https://krisumiwatersideprelaunch.in/sitemap.xml',
+  }
+}
